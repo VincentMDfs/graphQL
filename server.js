8684b1b719a423bc65d619893f050d236b86aaf7
plemire <p.lemire@outlook.com>
2020-08-05 10:54:13 -0400

    Question 2 work

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 
-// MySQL DB connection BEGIN
+// MySQL operational DB connection BEGIN
 var mysql = require('mysql');
 const con_mysql = mysql.createConnection({
   host: process.env.MYSQL_HOST,
@@ -17,14 +17,14 @@ const con_mysql = mysql.createConnection({
 });
 con_mysql.connect(function(error){
     if (!!error) {
-        console.log("\n" + "-- > NO!, connection to the MySQL DB "  + process.env.MYSQL_DB_NAME + " on HOST "+process.env.MYSQL_HOST + " disgracefully failed!" + "\n");
+        console.log("\n" + "-- > NO!, connection to the MySQL operational DB "  + process.env.MYSQL_DB_NAME + " on HOST "+process.env.MYSQL_HOST + " disgracefully failed!" + "\n");
     } else {
-        console.log("\n" + "-- > YES, connection to the MySQL DB " + process.env.MYSQL_DB_NAME + " on HOST "+process.env.MYSQL_HOST +" successfully established with success!." + "\n");
+        console.log("\n" + "-- > YES, connection to the MySQL operational DB " + process.env.MYSQL_DB_NAME + " on HOST "+process.env.MYSQL_HOST +" successfully established with success!." + "\n");
     }
 });
-// MySQL DB connection END
+// MySQL operational DB connection END
 
-// PostgreSQL DB connection BEGIN
+// PostgreSQL decision DB connection BEGIN
 const { Client }  = require('pg');
 const con_pg = new Client({
     user: process.env.POSTGRESQL_USERNAME,
@@ -35,36 +35,94 @@ const con_pg = new Client({
 });
 con_pg.connect(function(error){
     if (!!error) {
-        console.log("\n" + "-- > NO!, connection to the PostgreSQL DB "  + process.env.POSTGRESQL_DB_NAME + " on HOST "+process.env.POSTGRESQL_HOST + " disgracefully failed!" + "\n");
+        console.log("\n" + "-- > NO!, connection to the PostgreSQL decision DB "  + process.env.POSTGRESQL_DB_NAME + " on HOST "+process.env.POSTGRESQL_HOST + " disgracefully failed!" + "\n");
     } else {
-        console.log("\n" + "-- > YES, connection to the PostgreSQL DB " + process.env.POSTGRESQL_DB_NAME + " on HOST "+process.env.POSTGRESQL_HOST +" successfully established with success!." + "\n");
+        console.log("\n" + "-- > YES, connection to the PostgreSQL decision DB " + process.env.POSTGRESQL_DB_NAME + " on HOST "+process.env.POSTGRESQL_HOST +" successfully established with success!." + "\n");
     }
 });
-// PostgreSQL DB connection END
+// PostgreSQL decision DB connection END
 
 
 
 // GraphQL schema
 var schema = buildSchema(`
     type Query {
-        message: String
         buildings(id: Int!): Building
-        factintervention(building_id: Int!): FactIntervention
+        intervention(building_id: Int!): Intervention
+        employees(id: Int!): Employee
     },
 
     type Building {
         id: Int!
+        address: Address
+        customer_id: Int!
+        customer: Customer
         admin_full_name: String
         admin_email: String
+        admin_phone: String
         tech_contact_full_name: String
-    },
+        tech_contact_email: String
+        tech_contact_phone: String
+        building_details: [Building_detail]
+        interventions: [Intervention]
+    }
 
-    type FactIntervention {
+    type Building_detail {
         building_id: Int!
-        result: String
-        report: String
+        ikey: String
+        value: String
+    }
+
+    type Address {
+        type_of_address: String
+        status: String
+        entity: String
+        number_and_street: String
+        appartment_or_suite: String
+        city: String
+        state: String
+        zip_code: String
+        country: String
+        note: String
+        longitude: Float
+        latitude: Float
+    }
+
+    type Customer {
+        company_name: String
+        full_name: String
+        email: String
+        address_id: Int!
+        user_id: Int!
+        phone: String
+        company_description: String
+        tech_authority_full_name: String
+        tech_authority_email: String
+        tech_authority_phone: String
+    }
+
+    type Employee {
+        id: Int!
+        email: String
+        first_name: String
+        last_name: String
+        title: String
+        building_details: [Building_detail]
+        interventions: [Intervention]
+    }
+
+    type Intervention {
+        employee_id: Int!
+        building_id: Int!
+        battery_id: String
+        column_id: String
+        elevator_id: String
+        building_details: [Building_detail]
         start_date: String
         end_date: String
+        result: String
+        report: String
+        status: String
     }
 `);
 
@@ -73,26 +131,42 @@ var schema = buildSchema(`
 // Root resolver
 var root = {
     buildings: getBuildings,
-    factintervention: getFactIntervention
+    interventions: getInterventions,
 };
 
 
 async function getBuildings({id}) {
+    // Query building details from MySQL buildings table
     var buildings = await query_mysql('SELECT * FROM buildings WHERE id = ' + id )
     resolve = buildings[0]
 
+    // Query building details from PostgreSQL factintervention table
+    interventions = await query_postgresql('SELECT * FROM factintervention WHERE building_id = ' + id)
+
+    //Query customer details from MySQL buildings table
+    customer = await query_mysql('SELECT * FROM customers WHERE id = ' + resolve.customer_id)
+
+    resolve['customer']= customer[0];
+    resolve['interventions']= interventions;
+
     return resolve
 };
 
-async function getFactIntervention({building_id}) {
-    var factintervention = await query_postgresql('SELECT * FROM factintervention WHERE building_id = ' + building_id )
-    resolve = factintervention[0]
+async function getInterventions({building_id}) {
+    // Query intervention from PostgreSQL factintervention table
+    var intervention = await query_postgresql('SELECT * FROM factintervention WHERE building_id = ' + building_id)
+    resolve = intervention[0]
+    
+    // Query address from MySQL buildings table
+    address = await query_mysql('SELECT * FROM addresses WHERE entity = "Building" AND entity_id = ' + building_id)
+
+    resolve['address']= address[0];
 
     return resolve
 };
 
 
-// Function used to query the MySQL DB BEGIN
+// Function used to query the MySQL operational DB BEGIN
 function query_mysql (queryStr) {
     console.log("-- > Run MySQL query : " + queryStr)
     return new Promise((resolve, reject) => {
@@ -107,7 +181,7 @@ function query_mysql (queryStr) {
 // Function used to query the MySQL DB END
 
 
-// Function used to query the PostgreSQL DB BEGIN
+// Function used to query the PostgreSQL decision DB BEGIN
 function query_postgresql(queryStr) {
     console.log("-- > Run PostgreSQL query : " + queryStr)
     return new Promise((resolve, reject) => {
